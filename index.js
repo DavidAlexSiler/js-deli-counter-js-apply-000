786296326204f976f7fed59f67e452c8890c574a
Alex Siler <davidalexsiler@gmail.com>
2019-04-11 20:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var takeANumber = function(katzDeli, newCustomer){
-  katzDeli.push(newCustomer);
-  var welcomeMessage = `Welcome, ${newCustomer}. You are number ${katzDeli.length} in line.`;
-   return welcomeMessage;
-  };
+var newCustomerNumber = 0
+var takeANumber = function(katzDeli){
+  
+  newCustomerNumber ++;
+  katzDeli.push(newCustomerNumber);
+  var welcomeMessage = `Welcome. You are number ${newCustomerNumber}.`;
+  return welcomeMessage;
+};
+let line = []
+console.log(takeANumber(line))
+console.log(takeANumber(line))
+console.log(takeANumber(line))
+  
 var nowServing = function(katzDeliLine){
   if(katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine.shift()}.`;
@@ -10,18 +18,19 @@ var nowServing = function(katzDeliLine){
     return 'There is nobody waiting to be served!';
   }
 };
-var currentLine = function(katzDeliLine){
-var message = "The line is currently";
-var customerArr = [];
 
-if(katzDeliLine.length > 0){
-  katzDeliLine.forEach(function(element, index, array){
-   customerArr.push(`${index + 1}. ${element}`);
+var currentLine = function(katzDeliLine){
+  var message = "The line is currently";
+  var customerArr = [];
   
-  });
-   var customerArrToo = customerArr.join(', ');
-  return `${message}: ${customerArrToo}`;
-}else{
-  return `${message} empty.`;
-}
+  if(katzDeliLine.length > 0){
+    katzDeliLine.forEach(function(element, index, array){
+     customerArr.push(`${index + 1}. ${element}`);
+    
+    });
+     var customerArrToo = customerArr.join(', ');
+    return `${message}: ${customerArrToo}`;
+  }else{
+    return `${message} empty.`;
+  }
 };
